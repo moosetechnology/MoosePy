d7f14de71e6b8a16d690bccfdd1f003eae2dedc3
CyrilFerlicot <cyril@ferlicot.me>
2024-09-23 15:39:22 +0200

    [test project] Make project compilable + add non root import

--- resources/examples/project1/root/__init__.py
+++ resources/examples/project1/root/__init__.py
@@ -1,3 +1,1 @@
-import moduleAtRoot
-
 rootPackageVariable = 33
--- resources/examples/project1/root2/__init__.py
+++ resources/examples/project1/root2/__init__.py
@@ -1,8 +1,12 @@
 import root
 import root.subpackage1.subsubpackage1
 import root.subpackage1.moduleInSubPackage1
+import moduleAtRoot
 
 print(root.subpackage1.moduleInSubPackage1.moduleInSubpackage1Variable)
 
 print(root.rootPackageVariable)
 print(root.subpackage1.subsubpackage1.subSubPackage1Variable)
+print(moduleAtRoot.moduleAtRootVariable)
+
+rootPackage2Variable = 99
