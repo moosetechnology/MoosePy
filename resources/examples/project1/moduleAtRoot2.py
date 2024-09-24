ee541ec4a05535b46aea582db7b374b366aa2f5e
CyrilFerlicot <cyril@ferlicot.me>
2024-09-24 12:09:39 +0200

    [test project] Add import in class and methods

--- resources/examples/project1/moduleAtRoot2.py
+++ resources/examples/project1/moduleAtRoot2.py
@@ -1,5 +1,21 @@
 import moduleAtRoot
+import moduleAtRoot3
 
 moduleAtRoot2Variable = 42
 
 print(moduleAtRoot.moduleAtRootVariable)
+
+
+class Person:
+    import moduleAtRoot3
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def printInfos(self):
+        import moduleAtRoot4
+        if not moduleAtRoot3.moduleAtRootVariable3:
+            print(self.name + moduleAtRoot4.moduleAtRootSpaceString + str(self.age))
+
+p1 = Person("John", 36)
+p1.printInfos()
