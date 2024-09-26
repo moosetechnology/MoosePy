5859726955fa3c79cd67cd50f29010cf979ac640
CyrilFerlicot <cyril@ferlicot.me>
2024-09-26 18:07:31 +0200

    [test project] Add from import of entities in packages

--- resources/examples/project1/moduleAtRoot5.py
+++ resources/examples/project1/moduleAtRoot5.py
@@ -2,6 +2,8 @@ from root import rootPackageVariable
 from moduleAtRoot import moduleAtRootVariable
 from moduleAtRoot2 import Person
 from moduleAtRoot import sort_list
+from root.subpackage1.subsubpackage1 import Room
+from root.subpackage1.subsubpackage1 import return2
 
 print(rootPackageVariable)
 print(moduleAtRootVariable)
@@ -13,3 +15,13 @@ x = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 y = [0, 1, 1, 0, 1, 2, 2, 0, 1]
 
 print(sort_list(x, y))
+
+# create object of Room class
+study_room = Room()
+# assign values to all the properties
+study_room.length = 42.5
+study_room.breadth = 30.8
+# access method inside class
+study_room.calculate_area()
+
+print(return2())
--- resources/examples/project1/root/subpackage1/subsubpackage1/__init__.py
+++ resources/examples/project1/root/subpackage1/subsubpackage1/__init__.py
@@ -1,1 +1,14 @@
 subSubPackage1Variable = True
+
+# create a class
+class Room:
+    length = 0.0
+    breadth = 0.0
+
+    # method to calculate area
+    def calculate_area(self):
+        print("Area of Room =", self.length * self.breadth)
+
+
+def return2():
+    return 2
