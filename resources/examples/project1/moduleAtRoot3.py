ea9f0ec541ac613922b3d137c46aa04cbea5996e
CyrilFerlicot <cyril@ferlicot.me>
2024-09-26 18:03:02 +0200

    [test project] Add multiple from-imports

--- resources/examples/project1/moduleAtRoot3.py
+++ resources/examples/project1/moduleAtRoot3.py
@@ -1,1 +1,21 @@
 moduleAtRootVariable3 = False
+
+# create a class
+class Room:
+    length = 0.0
+    breadth = 0.0
+
+    # method to calculate area
+    def calculate_area(self):
+        print("Area of Room =", self.length * self.breadth)
+
+
+# create object of Room class
+study_room = Room()
+
+# assign values to all the properties
+study_room.length = 42.5
+study_room.breadth = 30.8
+
+# access method inside class
+study_room.calculate_area()
--- resources/examples/project1/moduleAtRoot5.py
+++ resources/examples/project1/moduleAtRoot5.py
@@ -1,3 +1,15 @@
 from root import rootPackageVariable
+from moduleAtRoot import moduleAtRootVariable
+from moduleAtRoot2 import Person
+from moduleAtRoot import sort_list
 
 print(rootPackageVariable)
+print(moduleAtRootVariable)
+
+p1 = Person("Imen", 37)
+p1.printInfos()
+
+x = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+y = [0, 1, 1, 0, 1, 2, 2, 0, 1]
+
+print(sort_list(x, y))
