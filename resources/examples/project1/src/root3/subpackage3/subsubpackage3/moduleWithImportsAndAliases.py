af94261807ebc23542eae4d1ea444ca3038b46c5
CyrilFerlicot <cyril@ferlicot.me>
2024-10-07 16:16:08 +0200

    [test project] Add examples of aliases in imports

---
+++ resources/examples/project1/src/root3/subpackage3/subsubpackage3/moduleWithImportsAndAliases.py
@@ -0,0 +1,10 @@
+import moduleAtRoot as mar
+import root.subpackage1.subsubpackage1 as rss
+from root2.moduleInRootPackage2 import Employee as Emp, functionWithImportsInRoot2 as fwi
+from moduleAtRoot2 import moduleAtRoot2Variable as marv
+
+print(mar.moduleAtRootVariable)
+print(rss.subSubPackage1Variable)
+print(Emp)
+print(fwi)
+print(marv)
