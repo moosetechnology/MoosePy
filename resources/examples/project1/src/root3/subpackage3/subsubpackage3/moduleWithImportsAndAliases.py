95fbede417511550d7cfb8df8d69bcc03b020e37
CyrilFerlicot <cyril@ferlicot.me>
2024-10-07 16:51:38 +0200

    [test project] Add non aliased name in a multiple import with aliases

--- resources/examples/project1/src/root3/subpackage3/subsubpackage3/moduleWithImportsAndAliases.py
+++ resources/examples/project1/src/root3/subpackage3/subsubpackage3/moduleWithImportsAndAliases.py
@@ -1,6 +1,6 @@
 import moduleAtRoot as mar
 import root.subpackage1.subsubpackage1 as rss
-from root2.moduleInRootPackage2 import Employee as Emp, functionWithImportsInRoot2 as fwi
+from root2.moduleInRootPackage2 import Employee as Emp, functionWithImportsInRoot2 as fwi, Employee2
 from moduleAtRoot2 import moduleAtRoot2Variable as marv
 
 print(mar.moduleAtRootVariable)
@@ -8,3 +8,4 @@ print(rss.subSubPackage1Variable)
 print(Emp)
 print(fwi)
 print(marv)
+print(Employee2)
