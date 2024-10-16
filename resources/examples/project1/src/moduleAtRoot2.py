bd5d14773c6694d16469b27573f646a1e19f13bc
CyrilFerlicot <cyril@ferlicot.me>
2024-10-16 15:35:43 +0200

    [test project] Add getter and setter

--- resources/examples/project1/src/moduleAtRoot2.py
+++ resources/examples/project1/src/moduleAtRoot2.py
@@ -9,6 +9,7 @@ print(moduleAtRoot.moduleAtRootVariable)
 
 class Person:
     import moduleAtRoot3
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -17,6 +18,13 @@ class Person:
         import moduleAtRoot4
         if not moduleAtRoot3.moduleAtRootVariable3:
             print(self.name + moduleAtRoot4.moduleAtRootSpaceString + str(self.age))
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
 
 p1 = Person("John", 36)
 p1.printInfos()
