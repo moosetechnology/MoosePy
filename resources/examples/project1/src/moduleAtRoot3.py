4d816c52054764dedef770870268453afd4005bb
CyrilFerlicot <cyril@ferlicot.me>
2024-10-23 15:37:20 +0200

    [test project] Add some code to better tests attributes

--- resources/examples/project1/src/moduleAtRoot3.py
+++ resources/examples/project1/src/moduleAtRoot3.py
@@ -2,7 +2,22 @@ moduleAtRootVariable3 = False
 
 class Dog:
 
-    kind = 'canine'         # class variable shared by all instances
+    kind = 'canine'         #class variable shared by all instances
 
     def __init__(self, name):
-        self.dog_name = name
+        self.dog_name = name    #Inst var declared in the init
+
+    def set_dog_color(self, color_name):
+        self.dog_color = color_name     #Inst var declared outside the init
+
+    sound = "woof"          #class var not at the beginning
+
+    def get_dog_color(self):
+        return self.dog_color
+
+
+dog = Dog("Stolys")
+dog.dog_color = "Black and blue"
+print(dog.dog_name)
+print(dog.get_dog_color())
+print(dog.__dict__)
