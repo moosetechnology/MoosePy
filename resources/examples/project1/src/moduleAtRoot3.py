035141ede666fe034764362efd76257946d767c6
CyrilFerlicot <cyril@ferlicot.me>
2024-10-23 16:10:04 +0200

    [test project] Add example code to test there is no source anchor duplication

--- resources/examples/project1/src/moduleAtRoot3.py
+++ resources/examples/project1/src/moduleAtRoot3.py
@@ -1,19 +1,26 @@
+# Please do not touch too much this file. We use the positions of the text to test source anchors
+
 moduleAtRootVariable3 = False
 
-class Dog:
 
-    kind = 'canine'         #class variable shared by all instances
+class Dog:
+    kind = 'canine'  # class variable shared by all instances
 
     def __init__(self, name):
-        self.dog_name = name    #Inst var declared in the init
+        self.dog_name = name  # Inst var declared in the init
 
     def set_dog_color(self, color_name):
-        self.dog_color = color_name     #Inst var declared outside the init
+        self.dog_color = color_name  # Inst var declared outside the init
 
-    sound = "woof"          #class var not at the beginning
+    def update_dog_color(self, color_name):
+        self.dog_color = color_name  # Second assignation to be sure we do not duplicate source anchors of ivars
+
+    sound = "woof"  # class var not at the beginning
 
     def get_dog_color(self):
         return self.dog_color
+
+    sound = "Waoof"  # Second assignation to be sure we do not duplicate source anchors of class vars
 
 
 dog = Dog("Stolys")
@@ -21,3 +28,7 @@ dog.dog_color = "Black and blue"
 print(dog.dog_name)
 print(dog.get_dog_color())
 print(dog.__dict__)
+Dog.newClassVar = 1
+print(dog.newClassVar)
+print(Dog.newClassVar)
+print(dog.sound)
