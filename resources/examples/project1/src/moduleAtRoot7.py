a4b7c97841873587f215a64ac7cd4075c1ae1c5b
CyrilFerlicot <cyril@ferlicot.me>
2024-10-24 16:09:14 +0200

    [test project] Add inner function

--- resources/examples/project1/src/moduleAtRoot7.py
+++ resources/examples/project1/src/moduleAtRoot7.py
@@ -1,3 +1,15 @@
 from root2 import NameOfSubpackageOrClass
 
 print(NameOfSubpackageOrClass)
+
+def function_with_inner_function():
+    result = 3
+
+    def inner_function(finput):
+        return finput + 2
+
+    result = result + inner_function(result)
+
+    return result
+
+print(function_with_inner_function())
