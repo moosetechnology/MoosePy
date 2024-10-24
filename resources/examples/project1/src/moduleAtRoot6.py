3469c9ee50af76d2a622ac2b21d2644c61e31290
CyrilFerlicot <cyril@ferlicot.me>
2024-10-24 15:24:50 +0200

    [test project] local variables of function all assigned at once

--- resources/examples/project1/src/moduleAtRoot6.py
+++ resources/examples/project1/src/moduleAtRoot6.py
@@ -1,3 +1,12 @@
 import root2.NameOfSubpackageOrClass
 
 print(root2.NameOfSubpackageOrClass)
+
+
+def function_with_multiple_assignations():
+    local_multiple1 = local_multiple2 = local_multiple3 = 2
+    local_multiple1 = 3
+    return local_multiple1 + local_multiple2 + local_multiple3
+
+
+print(function_with_multiple_assignations())
