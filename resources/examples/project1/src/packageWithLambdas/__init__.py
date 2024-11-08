7c6c541ab126e5703ba9509b4986052c5560f0be
CyrilFerlicot <cyril@ferlicot.me>
2024-11-08 15:33:15 +0100

    [test project] Add examples of lambdas

---
+++ resources/examples/project1/src/packageWithLambdas/__init__.py
@@ -0,0 +1,3 @@
+packageVariableLambda = lambda a: a + 10
+
+print(packageVariableLambda(10))
---
+++ resources/examples/project1/src/packageWithLambdas/moduleWithLambdas.py
@@ -0,0 +1,35 @@
+moduleVariableLambda = lambda a, b: a * b + 10
+
+print(moduleVariableLambda(10, 5))
+
+
+def function_returning_lambda(n):
+    return lambda a: a * n
+
+
+mydoubler = function_returning_lambda(2)
+mytripler = function_returning_lambda(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+
+class ClassWithLambda:
+    classVarWithLambda = lambda toto: print(toto.upper())
+
+    def method(self, param):
+        tempVar = lambda tata: not tata
+        return tempVar(param)
+
+
+c = ClassWithLambda()
+print(c.method(False))
+c.__class__.classVarWithLambda("cyril")
+
+
+def function_with_lambda_in_lambda(n):
+    mul = lambda x: (lambda y: y * x + n)
+    return mul(4)
+
+
+print(function_with_lambda_in_lambda(2)(4))
