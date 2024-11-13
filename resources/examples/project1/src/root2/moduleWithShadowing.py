e9b143427e39059d051f8cea5014ade6b6c1821e
CyrilFerlicot <cyril@ferlicot.me>
2024-11-13 16:15:21 +0100

    [test project] Add shadowed class

--- resources/examples/project1/src/root2/moduleWithShadowing.py
+++ resources/examples/project1/src/root2/moduleWithShadowing.py
@@ -7,3 +7,19 @@ class shadowedName:
 
 def shadowedName():
     print("I'm the last one so I win")
+
+
+class ClassShadowedByOtherClass:
+    c_var_of_shadowed_class = True
+
+
+print(ClassShadowedByOtherClass().__class__.__dict__)
+
+
+class ClassShadowedByOtherClass:
+
+    def __init__(self):
+        self.i_var_of_shadowed_class = 3
+
+
+print(ClassShadowedByOtherClass().__class__.__dict__)
