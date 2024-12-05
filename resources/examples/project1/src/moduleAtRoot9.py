e109e7960fee5007977931e016dfd345db9241ba
CyrilFerlicot <cyril@ferlicot.me>
2024-12-05 17:02:53 +0100

    [test project] Add multiple way to reference classes

--- resources/examples/project1/src/moduleAtRoot9.py
+++ resources/examples/project1/src/moduleAtRoot9.py
@@ -1,2 +1,26 @@
 class AbstractAnimal:
     name = ""
+
+
+class AClassToReference:
+    pass
+
+
+class AClassReferencingAClass:
+    cvar_referencing_class = AClassToReference
+
+    def method_referencing_class(self):
+        return AClassToReference
+
+
+def function_referencing_class():
+    return AClassToReference
+
+
+def function_with_lambda_referencing_class():
+    return lambda: AClassToReference
+
+
+print(function_referencing_class())
+print(AClassReferencingAClass().method_referencing_class())
+print(function_with_lambda_referencing_class()())
