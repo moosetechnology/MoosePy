1ec7508ff046ffa57cf180a24d35deff6fbea9a4
CyrilFerlicot <cyril@ferlicot.me>
2024-12-23 17:48:16 +0100

    [test project] Simple invocation

--- resources/examples/project1/src/moduleAtRoot.py
+++ resources/examples/project1/src/moduleAtRoot.py
@@ -12,14 +12,16 @@ print(moduleAtRootVariable)
 print(root.rootPackageVariable)
 print(root.subpackage1.subPackage1Variable)
 
-#============================================================
+
+# ============================================================
 # Python program to sort
 # one list using
 # the other list
 def sort_list(list1, list2):
-	zipped_pairs = zip(list2, list1)
-	z = [x for _, x in sorted(zipped_pairs)]
-	return z
+    zipped_pairs = zip(list2, list1)
+    z = [x for _, x in sorted(zipped_pairs)]
+    return z
+
 
 # driver code
 x = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
@@ -34,16 +36,25 @@ print(sort_list(x, y))
 
 print(sort_list)
 
-#============================================================
+# ============================================================
 
 import root2
+
 print(root2.rootPackage2Variable)
 
 
-#============================================================
+# ============================================================
 
 def functionWithImport():
-	import moduleAtRoot3
-	return not moduleAtRoot3.moduleAtRootVariable3
+    import moduleAtRoot3
+    return not moduleAtRoot3.moduleAtRootVariable3
+
 
 print(functionWithImport())
+
+
+def function_printing_arg(arg):
+    print(arg)
+
+
+function_printing_arg("Cyril")
