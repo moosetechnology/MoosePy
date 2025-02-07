808c4ebc9a52623d659d6df80f7c3cbe26e27058
CyrilFerlicot <cyril@ferlicot.fr>
2025-02-07 16:21:00 +0100

    [test project] Add invocation with alias

--- resources/examples/project1/src/root3/subpackage3/subsubpackage3/moduleWithImportsAndAliases.py
+++ resources/examples/project1/src/root3/subpackage3/subsubpackage3/moduleWithImportsAndAliases.py
@@ -4,6 +4,7 @@ from root2.moduleInRootPackage2 import Employee as Emp, functionWithImportsInRoo
 from moduleAtRoot2 import moduleAtRoot2Variable as marv
 
 print(mar.moduleAtRootVariable)
+print(mar.sort_list(["a", "b", "c", "d", "e", "f", "g", "h", "i"], [0, 1, 1, 0, 1, 2, 2, 0, 1]))
 print(rss.subSubPackage1Variable)
 print(Emp)
 print(fwi)
