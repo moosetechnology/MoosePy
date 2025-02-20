ca31a28eb693c66a179b40df39bf6b9056dc49df
CyrilFerlicot <cyril@ferlicot.fr>
2025-02-20 16:39:40 +0100

    [test project] add imports of packages

--- resources/examples/project1/src/moduleWithModulesAndPackageImport.py
+++ resources/examples/project1/src/moduleWithModulesAndPackageImport.py
@@ -1,11 +1,18 @@
 from root import moduleInRootPackage
+from root import subpackage1
 
 print(moduleInRootPackage.moduleInRootVariable)
+print(subpackage1.subPackage1Variable)
 
 
 def function_importing_module():
     from root.subpackage1 import moduleInSubPackage1
     return moduleInSubPackage1.moduleInSubpackage1Variable
+
+
+def function_importing_package():
+    from root2 import subpackageInRoot2
+    return subpackageInRoot2.function_returning_75
 
 
 class ClassImportingModule:
@@ -16,3 +23,13 @@ class ClassImportingModule:
     def method_importing_module(self):
         from root.subpackage1 import moduleInSubPackage1
         return moduleInSubPackage1.moduleInSubpackage1Variable
+
+
+class ClassImportingPackage:
+    from root2 import subpackageInRoot2
+
+    var_of_class_importing_package = subpackageInRoot2.function_returning_75()
+
+    def method_importing_package(self):
+        from root.subpackage1 import subsubpackage1
+        return subsubpackage1.return2()
