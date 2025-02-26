55bf152d821c3bfcaa4d0e34f2c4cfffeffcffac
CyrilFerlicot <cyril@ferlicot.fr>
2025-02-26 18:23:20 +0100

    [test project] Add regression test resource

--- resources/examples/project1/src/moduleAtRoot8.py
+++ resources/examples/project1/src/moduleAtRoot8.py
@@ -2,6 +2,7 @@ from moduleAtRoot import moduleAtRootVariable
 import root.subpackage1.subsubpackage1
 import root2.subpackageInRoot2
 import pandas as pd
+import numpy as np
 
 pd.read_csv("https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv")
 
@@ -28,3 +29,8 @@ class ClassUsingMetaclass(metaclass=CustomMetaclass):
 
 x = ClassUsingMetaclass()
 print(x.base)
+
+
+rnstate = np.random.RandomState(1)
+X = 10 * rnstate.rand(50)
+y = 2 * X - 5 + rnstate.randn(50)
