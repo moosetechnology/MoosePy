fe27cf0ca03a5091fc93bfbd86e47ed38d2d17af
CyrilFerlicot <cyril@ferlicot.fr>
2025-02-26 15:39:43 +0100

    [test project] add example of a metaclass manipulation

--- resources/examples/project1/src/moduleAtRoot8.py
+++ resources/examples/project1/src/moduleAtRoot8.py
@@ -1,6 +1,6 @@
 from moduleAtRoot import moduleAtRootVariable
 import root.subpackage1.subsubpackage1
-import root2 . subpackageInRoot2
+import root2.subpackageInRoot2
 import pandas as pd
 
 pd.read_csv("https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv")
@@ -11,5 +11,20 @@ print(root.subpackage1.subsubpackage1.function_with_function_ref())
 
 print(root2. subpackageInRoot2. function_returning_75())
 
+
 def functionReturningSomething():
-	return 8
+    return 8
+
+
+class CustomMetaclass(type):
+
+    def __init__(cls, name, bases, dct):
+        cls.base = 100
+
+
+class ClassUsingMetaclass(metaclass=CustomMetaclass):
+    pass
+
+
+x = ClassUsingMetaclass()
+print(x.base)
