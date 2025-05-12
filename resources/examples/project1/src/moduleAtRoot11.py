9086dfa7e5fa859240f3f68449d9847dde572418
CyrilFerlicot <cyril@ferlicot.fr>
2025-05-12 18:20:55 +0200

    [test project] Write access to array in a var

---
+++ resources/examples/project1/src/moduleAtRoot11.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Sample DataFrame with currency-formatted strings
+data = pd.DataFrame({
+    'Price': ['$1,000.00', '$2,500.50', '$3,750.99']
+})
+
+# Specify the column to clean
+curr_col = 'Price'
+
+# Remove dollar signs and commas
+data[curr_col] = data[curr_col].replace('[\$,]', '', regex=True)
+
+print(data)
