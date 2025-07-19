c3c060db48cda728033752a55d70dda0b49f87d2
CyrilFerlicot <cyril@ferlicot.fr>
2025-07-19 15:54:49 +0200

    [example project] Add break and continu statement

--- resources/examples/project1/src/moduleAtRoot8.py
+++ resources/examples/project1/src/moduleAtRoot8.py
@@ -39,3 +39,14 @@ class CustomMetaclass2(type):
 
     def __init__(cls, name, bases, dct):
         cls.base = 200
+
+
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
+
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
