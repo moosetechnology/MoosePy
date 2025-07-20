2cb801dfd5d089a9c71e8516ee209f348c7d563e
CyrilFerlicot <cyril@ferlicot.fr>
2025-07-20 15:23:31 +0200

    [exemple project] Add multiple assignations cases

---
+++ resources/examples/project1/src/moduleAtRoot13.py
@@ -0,0 +1,16 @@
+variableIdentifier = 34
+
+
+class ClassWithVariableAttribute:
+    variableAttribute = 35
+
+
+variableSubscript = [1, 2]
+variableSubscript[1] = 3
+
+
+(variableTuplePattern1, variableTuplePattern2) = 1, 2
+
+[variableListPattern1, variableListPattern2] = [1, 2]
+
+variablePatternListIdentifier1, variablePatternListIdentifier2 = 36, 37
