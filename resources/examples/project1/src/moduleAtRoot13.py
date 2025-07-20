4b0fe14e2e4819ff951f664cc1c27d7b5fd402c0
CyrilFerlicot <cyril@ferlicot.fr>
2025-07-20 16:06:42 +0200

    [example project] Add more augmented assignments tests

--- resources/examples/project1/src/moduleAtRoot13.py
+++ resources/examples/project1/src/moduleAtRoot13.py
@@ -8,9 +8,20 @@ class ClassWithVariableAttribute:
 variableSubscript = [1, 2]
 variableSubscript[1] = 3
 
-
 (variableTuplePattern1, variableTuplePattern2) = 1, 2
 
 [variableListPattern1, variableListPattern2] = [1, 2]
 
 variablePatternListIdentifier1, variablePatternListIdentifier2 = 36, 37
+
+variableAugmentedAssignmentIdentifier = 1
+variableAugmentedAssignmentIdentifier += 4
+
+
+class ClassWithAugmentedAssignment:
+    variableAugmentedAssignmentAttribute = 2
+    variableAugmentedAssignmentAttribute -= 3
+
+
+variableAugmentedAssignmentSubscript = [1, 2]
+variableAugmentedAssignmentSubscript[1] += 3
