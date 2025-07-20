23a805eab96a27eb63cddbbdce27da656783b05e
CyrilFerlicot <cyril@ferlicot.fr>
2025-07-20 16:30:45 +0200

    [example project] Add management of named expression

--- resources/examples/project1/src/moduleAtRoot13.py
+++ resources/examples/project1/src/moduleAtRoot13.py
@@ -25,3 +25,26 @@ class ClassWithAugmentedAssignment:
 
 variableAugmentedAssignmentSubscript = [1, 2]
 variableAugmentedAssignmentSubscript[1] += 3
+
+while (namedExpressionVariableInWhileCondition := input("Enter something (empty to quit): ")) != "":
+    print(f"You entered: {namedExpressionVariableInWhileCondition}")
+
+if (namedExpressionVariableInIfCondition := input("What's your name? ")) != "":
+    print(f"Hello, {namedExpressionVariableInIfCondition}!")
+
+print(f"The double of value is {(namedExpressionVariableInInterpolation := 2)}")
+
+# Assign and compare in a single expression
+if ((namedExpressionVariableInParenthesizedExpression := len([1, 2, 3])) > 2):
+    print(f"List is long enough ({namedExpressionVariableInParenthesizedExpression} elements)")
+
+
+def function_to_be_called_with_named_expression(length):
+    print(f"Length is {length}")
+
+
+# Named expression in argument list
+function_to_be_called_with_named_expression((namedExpressionVariableInArgumentList := len("hello")))
+
+# Named expression inside a tuple
+tupleWithNamedExpression = ((namedExpressionVariableInTuple := 1), (y := 2), namedExpressionVariableInTuple + y)
