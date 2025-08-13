03575e9db0dffb96e453fc7248b825a39a201452
CyrilFerlicot <cyril@ferlicot.fr>
2025-08-13 15:44:43 +0200

    [example project] Add code for a regression test

--- resources/examples/project1/src/moduleAtRoot7.py
+++ resources/examples/project1/src/moduleAtRoot7.py
@@ -17,3 +17,17 @@ def function_with_inner_function():
     return result
 
 print(function_with_inner_function())
+
+
+dataB = {
+    'A': ['foo', 'bar', 'baz', 'qux'],
+    'B': [1, 2, 3, 4],
+    'C': ['cat', 'dog', 'cat', 'dog']
+}
+
+df = pandas.DataFrame(dataB)
+
+# Convert column 'C' to a categorical type
+col = 'C'
+# The next line is there to ensure we can find that a node is on left side of an assignation in a node that have a field containing multiple nodes
+df.loc[:, col] = df[col].astype('category')
