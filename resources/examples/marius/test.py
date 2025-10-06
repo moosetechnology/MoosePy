fba97c46c2f831a0cfa17560714a6c09069893f8
JMLF <marius.mignard@inria.fr>
2025-10-06 15:12:31 +0200

    test ressources

--- resources/examples/marius/test.py
+++ resources/examples/marius/test.py
@@ -166,3 +166,37 @@ def local_func(aParameter):
 
 # Second invocation of fit, one of the candates should be from the local class
 model.fit(X_train, Y_train, batch_size=32, epochs=300, validation_data=(X_val, Y_val))
+
+
+def add_datepart(
+    df: pd.DataFrame,
+    field_name: str,
+    prefix: str = None,
+    drop: bool = True,
+    time: bool = True,
+    date: bool = True,
+):
+    """
+    Helper function that adds columns relevant to a date in the column `field_name` of `df`.
+    from fastai: https://github.com/fastai/fastai/blob/master/fastai/tabular/transform.py#L55
+    """
+    field = df[field_name]
+    prefix = ifnone(prefix, re.sub("[Dd]ate$", "", field_name))
+    attr = [
+        "Year",
+        "Month",
+        "Week",
+        "Day",
+        "Dayofweek",
+        "Is_month_end",
+        "Is_month_start",
+    ]
+    if date:
+        attr.append("Date")
+    if time:
+        attr = attr + ["Hour", "Minute"]
+    for n in attr:
+        df[prefix + n] = getattr(field.dt, n.lower())
+    if drop:
+        df.drop(field_name, axis=1, inplace=True)
+    return df
