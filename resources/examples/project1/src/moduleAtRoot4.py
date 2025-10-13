fc10411fdc552bd27b80facfbc16dfd36f47343a
CyrilFerlicot <cyril@ferlicot.fr>
2025-10-13 14:44:41 +0200

    example project: Add regression test code

--- resources/examples/project1/src/moduleAtRoot4.py
+++ resources/examples/project1/src/moduleAtRoot4.py
@@ -9,3 +9,9 @@ matplotlib.pyplot.plot(xpoints, ypoints)
 moduleAtRootSpaceString = ' '
 
 numpy.random.random(50000)**2
+
+
+# next two lines are for a regression test
+from tensorflow.python.ops import variable_scope
+
+variable_scope.variable_scope()
